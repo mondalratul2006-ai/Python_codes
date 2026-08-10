7989d8c057941a311ff46d171fd3619ce0248665
mondalratul2006-ai <mondalratul2006@gmail.com>
2026-08-10 20:32:01 +0530

    Create distance1.py

---
+++ Basic/distance1.py
@@ -0,0 +1,7 @@
+#distance of a line
+x1=int(input("Enter the X1 Co-ordinate = "))
+y1=int(input("Enter the Y1 Co-ordinate = "))
+x2=int(input("Enter the X2 Co-ordinate = "))
+y2=int(input("Enter the Y2 Co-ordinate = "))
+d=((x2-x1)**2+(y2-y1)**2)**0.5
+print("Distance = ",d)
